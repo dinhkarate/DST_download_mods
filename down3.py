08bbb53a7bcc74a73bbfd93322eab6475d253539
pandinh2007 <57869841+pandinh2007@users.noreply.github.com>
2024-07-27 23:12:37 +0700

    1.3.2

--- down3.py
+++ down3.py
@@ -5,6 +5,12 @@ from tkinter import filedialog, messagebox
 import os
 import sys
 
+messagebox.showinfo("Chọn đường dẫn Steam", "Chọn đường dẫn Steam")
+steamapps = filedialog.askdirectory(title = "Chọn đường dẫn Steam")
+mod_dir = "steamapps/workshop/content/322330"
+steamapps_mod = f'{steamapps}/{mod_dir}'
+#print(steamapps)
+#print(steamapps_mod)
 
 mod_id_list = []
 
@@ -69,9 +75,9 @@ def process_file():
                             t = match3.group(1)
                     
                     #batch_file.write(' +quit\n')
-            messagebox.showinfo("Mod Information", f"Mod bị lỗi:\nMod Name: {x}\nServer Version: {y}\nWorkshop Version: {z}\nMã lỗi: {t}")
-            url = f'https://www.google.com/search?q={x}'
-            webbrowser.open(url)
+            # messagebox.showinfo("Mod Information", f"Mod bị lỗi:\nMod Name: {x}\nServer Version: {y}\nWorkshop Version: {z}\nMã lỗi: {t}")
+            # url = f'https://www.google.com/search?q={x}'
+            # webbrowser.open(url)
             # messagebox.showinfo("Thông báo", "Các mod bị lỗi đã được hiển thị trong trình duyệt.\nChạy file download_mods.bat để tự động tải về.\nFile batch đã được tạo thành công.")
         except OSError as e:
             messagebox.showerror("Lỗi", f"Đã xảy ra lỗi khi xử lý file: {e}")
@@ -96,7 +102,7 @@ def option2_action():
     if mod_id_list:
         output_batch_file = "download_mods.bat"
         with open(output_batch_file, 'w', encoding='utf-8') as batch_file:
-            batch_file.write(f'"{steamcmd_path}" +login anonymous')
+            batch_file.write(f'"{steamcmd_path}" +force_install_dir "{steamapps_mod}" +login anonymous')
             for mod_id in mod_id_list:
                 batch_file.write(f' +workshop_download_item 322330 {mod_id} validate')
             batch_file.write(' +quit')
@@ -104,19 +110,44 @@ def option2_action():
     else:
         messagebox.showinfo("Lỗi", "Không có id nào")
 def option3_action():
-    for mod_id in mod_id_list:
-        print(mod_id)
+    popup = tk.Toplevel(root)
+    popup.geometry("300x200")
+    popup.title("Thêm ID mod")
 
+    label1 = tk.Label(popup, text="Nhập ID mod:")
+    label1.pack(pady=5)
+    
+    entry1 = tk.Entry(popup)
+    entry1.pack(pady=5)
+
+    def xacnhan_button():
+        id_mod = entry1.get()
+        mod_id_list.append(id_mod)
+        popup.destroy()
+
+    xacnhan = tk.Button(popup, text="Xác nhận", command=xacnhan_button)
+    xacnhan.pack(pady=10)
+
+def download_steamcmd():
+    url = "https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip"
+    webbrowser.open(url)
+    messagebox.showinfo("", "Tải về xong giải nén vào thư mục chứa script rồi\nSau đó chạy steamcmd để nó cài đặt")
+
+def choose_steam_dir():
+    steamapps = filedialog.askdirectory(title = "Chọn đường dẫn Steam")
+    steamapps_mod = f'"{steamapps}/{mod_dir}"'
 
 root = tk.Tk()
 root.title("SteamCMD Mod Downloader")
 
 process_button = tk.Button(root, text="Chọn file log", command=process_file)
-process_button.pack(pady=20)
+process_button.pack(pady=10)
 
+process_button1 = tk.Button(root, text="Tải steamcmd", command=download_steamcmd)
+process_button1.pack(pady=10)
 
-label = tk.Label(root, text="Tuỳ chọn cho các mod game không auto tải:")
-label.pack(pady=10)
+process_button2 = tk.Button(root, text="Chọn lại đường dẫn Steam", command=choose_steam_dir)
+process_button2.pack(pady=10)
 
 button_frame = tk.Frame(root)
 button_frame.pack(pady=10)
@@ -127,7 +158,7 @@ option1_button.pack(side=tk.LEFT, padx=5)
 option2_button = tk.Button(button_frame, text="Tạo file download_mods", command=option2_action)
 option2_button.pack(side=tk.LEFT, padx=5)
 
-option3_button = tk.Button(button_frame, text="In list id không tìm thấy", command=option3_action)
+option3_button = tk.Button(button_frame, text="Thêm Mod", command=option3_action)
 option3_button.pack(side=tk.LEFT, padx=5)
 
 root.mainloop()
